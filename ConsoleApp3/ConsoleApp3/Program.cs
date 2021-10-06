9759d9d77a8c28497b45dfb6dda46eb015a08558
Alaxander0Yusov <yusovsky2@gmail.com>
2021-10-06 21:26:25 +0300

    first project

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ax^2+bx+c=0;
+            double a = Convert.ToDouble (Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            double dis = 
+            
+            Math.Sqrt()
+
+
+
+        }
+    }
+}
