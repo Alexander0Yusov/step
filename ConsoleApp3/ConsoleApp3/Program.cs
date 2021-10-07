41908c2402acef9833c9affeb5663c89ec2549e1
Alaxander0Yusov <yusovsky2@gmail.com>
2021-10-07 21:18:25 +0300

    refresh

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -11,14 +11,45 @@ namespace ConsoleApp3
         static void Main(string[] args)
         {
             // ax^2+bx+c=0;
+            //string insert = Console.ReadLine();
+            double x1, x2;
+
+            Console.WriteLine("Введите значение переменной a: ");
             double a = Convert.ToDouble (Console.ReadLine());
+
+            Console.WriteLine("Введите значение переменной b: ");
             double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение переменной c: ");
             double c = Convert.ToDouble(Console.ReadLine());
 
-            double dis = 
-            
-            Math.Sqrt()
+            double dis = b * b - 4 * a * c;
 
+            double root = Math.Sqrt(dis);
+            
+            if (root > 0)
+            {
+                x1 = (-b - root) / (2 * a);
+                x2 = (-b + root) / (2 * a);
+                Console.WriteLine("x1: ");
+                Console.WriteLine( Convert.ToString(x1));
+                Console.WriteLine("x1: ");
+                Console.WriteLine(Convert.ToString(x2));
+            }
+
+            if (root == 0)
+            {
+                x1 = (-b - root) / (2 * a);
+                Console.WriteLine("x: ");
+                Console.WriteLine(Convert.ToString(x1));
+            }
+
+            if (root < 0)
+            {
+                Console.WriteLine("Нет корней");
+            }
+
+            Console.ReadKey();
 
 
         }
